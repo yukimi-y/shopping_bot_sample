f3baa55275f29551662c874ad0881e525adaa8f4
yukimi.y <yyy.yukimi@gmail.com>
2021-05-24 18:49:06 +0900

    コントローラー実装

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,2 +1,126 @@
 class LinebotsController < ApplicationController
+  require 'line/bot'
+
+  protect_from_forgery except: [:callback]
+
+  def callback
+    body = reqiest.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::MessageType::Text
+          input = event.message['text']
+          message = search_and_create_message(input)
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
+    end
+  end
+
+  def search_and_create_message(input)
+    RakutenWebService.configure do |c|
+      c.application_id = ENV['RAKUTEN_APPID']
+      c.affiliate_id = ENV['RAKUTEN_AFID']
+    end
+
+    res = RakutenWebService::Item.search(keyword: input, hits: 3, imageFlag: 1)
+    items = []
+
+    items = res.map{|item| item}
+    make_reply_content(items)
+  end
+
+  def make_reply_content(items)
+    {
+      "type": 'flex',
+      "altText": 'This is a Flex Message',
+      "contents": 
+      {
+        "type": 'carousel',
+        "contents": [
+          make_part(items[0]),
+          make_part(items[1]),
+          make_part(items[2])
+        ]
+      }
+    }
+  end
+
+  def make_part(item)
+    title = item['itemName']
+    price = item['itemPrice'].to_s + '円'
+    url = item['itemUrl']
+    image = item['mediumImageUrls'].first
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": "image"
+      },
+      "body":
+      {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "title",
+            "wrap": "true",
+            "weight": "bold",
+            "size": "lg"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "text",
+                "text": "price",
+                "wrap": "true",
+                "weight": "bold",
+                "flex": 0
+              }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "楽天市場商品ページへ",
+              "uri": url  
+            }
+          }
+        ]
+      }
+    }
+  end
 end
